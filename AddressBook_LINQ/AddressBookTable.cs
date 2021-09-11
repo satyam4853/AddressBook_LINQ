325d02edd48124e00d1a4aa6743476431054c686
satyam4853 <vaishnavsatyam48532gmail.com>
2021-09-12 00:17:44 +0530

    [satyam] feat:Welcome Message

---
+++ AddressBook_LINQ/AddressBookTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBook_LINQ
+{
+   public class AddressBookTable
+    {
+        public void CreateAddressBookDataTable()
+        {
+            //DataTable 
+            DataTable table = new DataTable();
+
+            //Add Columns
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(long));
+            table.Columns.Add("Email", typeof(string));
+        }
+    }
+}
